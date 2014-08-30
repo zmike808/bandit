9878f14e89aabe35ca78b638a9f4334c385ebf80
Michael Zemsky <zmike808@gmail.com>
2014-08-30 14:14:13 -0400

    Update btsample.py
    functional example of how to ensure torrent file intrigrity

--- bandit-server/btsample.py
+++ bandit-server/btsample.py
@@ -1,52 +1,24 @@
-#!/bin/python
-
+from bcoding import bencode, bdecode
+import hashlib
 import sys
-import os
-import libtorrent
 
 if len(sys.argv) < 3:
-	print 'usage make_torrent.py file tracker-url'
-	sys.exit(1)
-
-input = os.path.abspath(sys.argv[1])
-
-fs = libtorrent.file_storage()
-
-#def predicate(f):
-#	print f
-#	return True
-#libtorrent.add_files(fs, input, predicate)
-
-parent_input = os.path.split(input)[0]
-
-for root, dirs, files in os.walk(input):
-	# skip directories starting with .
-	if os.path.split(root)[1][0] == '.': continue
-
-	for f in files:
-		# skip files starting with .
-		if f[0] == '.': continue
-
-		# skip thumbs.db on windows
-		if f == 'Thumbs.db': continue
-
-		fname = os.path.join(root[len(parent_input)+1:], f)
-		size = os.path.getsize(os.path.join(parent_input, fname))
-		print '%10d kiB  %s' % (size / 1024, fname)
-		fs.add_file(fname, size);
-
-if fs.num_files() == 0:
-	print 'no files added'
-	sys.exit(1)
-
-t = libtorrent.create_torrent(fs, 0, 4 * 1024 * 1024)
-
-t.add_tracker(sys.argv[2])
-t.set_creator('libtorrent %s' % libtorrent.version)
-
-libtorrent.set_piece_hashes(t, parent_input, lambda x: sys.stderr.write('.'))
-sys.stderr.write('\n')
-
-f = open('out.torrent', 'wb+')
-print >>f, libtorrent.bencode(t.generate())
-f.close()
+  print('enter torrent filename and filename')
+  exit()
+with open(sys.argv[1], 'rb') as f:
+  torrent = bdecode(f)
+  plength = torrent['info']['piece length']
+  thash = torrent['info']['pieces']
+  realfile = open(sys.argv[2], 'rb')
+  piece = realfile.read(plength)
+  digest = hashlib.sha1(piece).digest()
+  while piece != bytes():
+    piece = realfile.read(plength)
+    if piece != bytes():
+      phash = hashlib.sha1(piece).digest()
+      digest = digest + phash
+  if digest == thash:
+    print("file integrity is intact!!")
+  else:
+    print("kid, what are you trying to pull? get that malicious shit outta here")
+  realfile.close()
